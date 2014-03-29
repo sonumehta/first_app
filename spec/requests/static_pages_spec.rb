221661348e4d32f318eed7c9c95b43daa2d7c277
Sonu Mehta <sonumehta@iitj.ac.in>
2014-03-29 22:12:14 +0530

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,49 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'ShopMore'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('ShopMore')
+    end
+	 it "should have the title 'Home'" do
+	  visit '/static_pages/home'
+	  expect(page).to have_title("ShopMore-Online shopping website | Home")
+	end
+
+
+
+  end
+
+
+
+describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("ShopMore-Online shopping website | Help")
+    end
+
+
+
+  end
+
+describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/aboutus'
+      expect(page).to have_content('About Us')
+	end
+it "should have the title 'About Us'" do
+      visit '/static_pages/aboutus'
+      expect(page).to have_title("ShopMore-Online shopping website | About Us")
+   
     end
   end
 end
