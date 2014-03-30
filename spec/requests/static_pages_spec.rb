304927124c9df1a3063cbb966747b30edb033378
Sonu Mehta <sonumehta@iitj.ac.in>
2014-03-30 09:47:41 +0530

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,16 +5,16 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'ShopMore'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('ShopMore')
     end
 	it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("ShopMore-Online shopping website")
     end
 
 it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
 
@@ -22,15 +22,28 @@ it "should not have a custom page title" do
   end
 
 
+describe "Contact page" do
 
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("ShopMore-Online shopping website | Contact")
+    end
+
+
+end
 describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("ShopMore-Online shopping website | Help")
     end
 
@@ -41,11 +54,11 @@ it "should have the title 'Help'" do
 describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/aboutus'
+      visit aboutus_path
       expect(page).to have_content('About Us')
 	end
 it "should have the title 'About Us'" do
-      visit '/static_pages/aboutus'
+      visit aboutus_path
       expect(page).to have_title("ShopMore-Online shopping website | About Us")
    
     end
